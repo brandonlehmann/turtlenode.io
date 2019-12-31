7d4503a500c68a5c13fb98700899cb6e15dbaefb
Brandon Lehmann <brandonlehmann@gmail.com>
2019-12-31 10:42:26 -0500

    US-EAST deprecated

--- js/config.js
+++ js/config.js
@@ -1,5 +1,4 @@
 const apiInterface = 'https://api.turtlenode.io/'
 const nodes = [
-  { name: 'US-East-1', hostname: 'node-1.nyc.turtlenode.io', port: 11898, region: 'US East' },
   { name: 'SEED', hostname: 'seed.turtlenode.io', port: 11898, region: 'US East' }
 ]
